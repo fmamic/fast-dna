3f90c2fb12c44b1fef088125e722784d6234ef0b
nicholasrice <nicholas.rice119@gmail.com>
2018-12-05 23:16:57 -0800

    add test for focus-visible

--- packages/fast-components-styles-msft/src/checkbox/index.ts
+++ packages/fast-components-styles-msft/src/checkbox/index.ts
@@ -30,11 +30,6 @@ const styles: ComponentStyles<CheckboxClassNameContract, DesignSystem> = (
     const checkboxColor: any = toggleFieldPattern.rest.stateIndicator.checked.background(
         designSystem
     );
-    // const checkboxColor: string = normalContrast(
-    //     designSystem.contrast,
-    //     foregroundColor,
-    //     backgroundColor
-    // );
     const checkboxDisabled: string = disabledContrast(
         designSystem.contrast,
         foregroundColor,
@@ -75,6 +70,7 @@ const styles: ComponentStyles<CheckboxClassNameContract, DesignSystem> = (
             flexShrink: "0",
             "&::before, &::after": {
                 content: "''",
+                pointerEvents: "none",
                 width: "2px",
                 position: "absolute",
                 zIndex: "1",
@@ -115,7 +111,7 @@ const styles: ComponentStyles<CheckboxClassNameContract, DesignSystem> = (
         checkbox__indeterminate: {
             "& $checkbox_stateIndicator": {
                 "&::before": {
-                    borderRadius: "2px",
+                    borderRadius: toPx(designSystem.cornerRadius),
                     transform: "none",
                     [applyLocalizedProperty("left", "right", direction)]: "5px",
                     top: "5px",
